e1789d2f233cef75e90c084033e1b316ee385631
João Augusto Grobe Castilho <joaocastilho@alunos.utfpr.edu.br>
2019-02-09 15:24:42 -0200

    Style - Nav
    Definindo style do component Nav
    Alterado tamanho de fonte do footer

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -3,5 +3,13 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-        Navegação
+        <nav className="menu">
+            {/* Nav-item jsx   Refatorar !? */}
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+        </nav>
     </aside>
